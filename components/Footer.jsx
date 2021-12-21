64e95cf868ef6263e7c8e27d66fd6838afd3d568
Lavish Goyal <goellavish10@gmail.com>
2021-12-21 22:18:25 +0530

    footer

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,25 +1,45 @@
 import React from "react";
 import styled from "styled-components";
+import { FaDiscord } from "react-icons/fa";
+import { FaLinkedin } from "react-icons/fa";
+import { FaInstagram } from "react-icons/fa";
+import { FaYoutube } from "react-icons/fa";
+import { FaFacebook } from "react-icons/fa";
+import { FaGithub } from "react-icons/fa";
+import { MdOutlineEmail } from "react-icons/md";
 
 const Wrapper = styled.div`
     font-family: 'Roboto', sans-serif;
     background: #0F0F0F;
     padding: 30px 100px;
     color: white;
+    @media(max-width:560px){
+        display: flex;
+        padding: 30px 50px;
+    }
 `;
 
 const Content = styled.div`
     display: flex;
-    gap: 450px;
+    gap: 350px;
+    @media(max-width:560px){
+        flex-direction: column;
+        gap: 0;
+    }
 `;
 
 const Contact = styled.div`
     display: flex;
+    flex-direction: column;
 `;
 
 const Links = styled.div`
     display: flex;
     gap: 200px;
+    @media(max-width:560px){
+        flex-direction: column;
+        gap: 0;
+    }
 `;
 
 const Explore = styled.div`
@@ -33,6 +53,12 @@ const About = styled.div`
 const Hrefs = styled.div`
     display: flex;
     flex-direction: column;
+`;
+
+const Icons = styled.div`
+    display: flex;
+    flex-direction: row;
+    gap: 15px;
 `;
 
 export const Footer = () => {
@@ -60,6 +86,15 @@ export const Footer = () => {
                 </Links>
                 <Contact>
                     <h2>Contact Us</h2>
+                    <Icons>
+                        <a href="#" style={{ color: "#5865F2" }}><FaDiscord /></a>
+                        <a href="#" style={{ color: "#0e76a8", background: "#fff", height: '15px', width: '16px' }}><FaLinkedin /></a>
+                        <a href="#" style={{ color: "#cd486b" }}><FaInstagram /></a>
+                        <a href="#" style={{ marginBottom: '20px', fontWeight: 'normal' }}><FaGithub /></a>
+                        <a href="#" style={{ color: "#FF0000" }}><FaYoutube /></a>
+                        <a href="#" style={{ color: "#4267B2" }}><FaFacebook /></a>
+                        <a href="#" style={{ marginBottom: '20px', fontWeight: 'normal' }}><MdOutlineEmail /></a>
+                    </Icons>
                 </Contact>
             </Content>
         </Wrapper>
